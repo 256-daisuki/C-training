b19b1886a0424df6ba7ca857b4de37190566e776
256-daisuki <256mitdaisuki+git@gmail.com>
2023-07-27 07:25:46 +0900

    save

--- c-prn/main.c
+++ c-prn/main.c
@@ -5,7 +5,7 @@
 #include <ctype.h>
 #include <math.h>
 
-#define STACK_SIZE 50 //doubleの大きさ
+#define STACK_SIZE 50 // doubleの大きさ
 
 typedef struct {
     double data[STACK_SIZE];
@@ -13,34 +13,38 @@ typedef struct {
 } Stack;
 
 void initStack(Stack* stack) {
-    stack->top = -1;
+    stack->top = -1; // 初期化 元が-1らしい(ChatGPT情報)
 }
 
-bool push(Stack* stack, double value) {
+bool push(Stack* stack, double value) { // topにまだスタックを積めるか確認
     return stack->top < STACK_SIZE - 1 ? (stack->data[++stack->top] = value, true) : false;
 }
 
-bool pop(Stack* stack, double* value) {
+bool pop(Stack* stack, double* value) { // topがnot 空だったらデータを取り出してポインタに格納するやつ
     return stack->top >= 0 ? (*value = stack->data[stack->top--], true) : false;
 }
 
 int main() {
-    printf("helpでヘルプを表示 qで終了\n");
+    printf("hでヘルプを表示 qで終了\n");
     Stack stack;
     initStack(&stack);
     
     char input[100];
     double result;
-    int help_num = 0;
-    
+    int help_num = 0; // helpを表示したときに計算結果が表示されないようにするやつ    
     while (true) {
         fgets(input, sizeof(input), stdin);
         
-        if (input[0] == 'q') //qが押されたら終わらせる処理
+        if (input[0] == 'q') // qが押されたら終わらせる
             break;
         
-        if (input[0] == 'h') {
+        if (input[0] == 'h') { // hが押されたらヘルプを表示
             printf("逆ポーランド記法は、1+1 を 1 1 + と入力する書き方だよ　()を使わないから早く打てたりするらしい　電卓はHPぐらいしか作ってなかったけどね　詳しくは自分で調べて\n操作方法 + - * / のよくあるやつで四則演算 ^でx乗 rでルートを使えるよ\n");
+            help_num++;
+        }
+
+        if (input[0] == 'p') {
+            printf("qで終了だよ！pじゃないよ！！\n");
             help_num++;
         }
         
@@ -52,8 +56,13 @@ int main() {
             } else if (strchr("+-*/^r", *token) != NULL) { // 文字一覧（？）
                 double a, b;
                 pop(&stack, &b);
-                if (*token == 'r') {
-                    push(&stack, sqrt(b));
+                if (*token == 'r') { // ルートの処理
+                    if (b >= 0) {
+                        push(&stack, sqrt(b));
+                    } else {
+                        printf("エラー：虚数に対応してません\n");
+                        help_num++;
+                    }
                 } else {
                     pop(&stack, &a);
                     switch (*token) {
@@ -76,7 +85,7 @@ int main() {
             token = strtok(NULL, " ");
         }
         pop(&stack, &result);
-        if (help_num == 1){
+        if (help_num == 1) { // help_numが1なら計算結果を表示しないでhelp_numを0にする　それ以外なら普通に結果を表示
             help_num = 0;
         } else {
             printf("結果: %f\n", result);
---
+++ c-prn/tempCodeRunnerFile.c
@@ -0,0 +1,5 @@
+                    if (b >= 0) {
+                            push(&stack, sqrt(b));
+                        } else {
+                            printf("エラー：負の数のルートは虚数解になります\n");
+                        }
